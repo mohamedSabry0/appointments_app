2f5ce0d0b18e62d6843f888f542eadda31c2fc94
MohammadYaser <yasersafi19@outlook.com>
2024-01-29 19:40:19 +0430

    fix linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :consultations, dependent: :destroy
-    has_many :engineers, through: :consultations
+  has_many :consultations, dependent: :destroy
+  has_many :engineers, through: :consultations
 end
