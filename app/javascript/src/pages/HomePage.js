2278e6a306b6d32feb64d304b892af6d99408e59
MohammadYaser <yasersafi19@outlook.com>
2024-01-31 20:12:42 +0430

    add new component for sidebar

--- app/javascript/src/pages/HomePage.js
+++ app/javascript/src/pages/HomePage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import EngineersList from '../components/EngineersList';
+import Sidebar from './Sidebar';
 
 const HomePage = () => (
+  <>
+  <Sidebar />
   <div className="home-main">
     <h1>Available Engineers</h1>
     <p>Choose an engineer to book a consultation with</p>
     <EngineersList />
-  </div>
+    </div>
+    </>
 );
 
 export default HomePage;
---
+++ app/javascript/src/pages/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <button className='btn btn-danger'>send</button>
+    </aside>
+  )
+}
+
+export default Sidebar
