e52f2b03f2dcc9c3cd6cd8375cb0ae5738b61970
mohamedSabry0 <mohamedsabry3023@gmail.com>
2024-02-01 17:48:22 +0200

    Refactor all controllers for better responses

--- app/controllers/api/v1/consultations_controller.rb
+++ app/controllers/api/v1/consultations_controller.rb
@@ -1,7 +1,28 @@
 class Api::V1::ConsultationsController < ApplicationController
-  def index; end
+  def index
+    @consultations = Consultation.select(:id, :user_id, :engineer_id, :city, :date).all
+    if @consultations.empty?
+      render json: { message: 'No consultations found' }
+    else
+      render json: @consultations
+    end
+  end
 
-  def create; end
+  def create
+    @consultation = Consultation.new(consultation_params)
+    if @consultation.save
+      render json: @consultation
+    else
+      render json: { error: 'Unable to create consultation' }
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @consultation = Consultation.find(params[:id])
+    if @consultation.destroy
+      render json: { message: 'Consultation deleted' }
+    else
+      render json: { error: 'Unable to delete consultation' }
+    end
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { user: @user, logged_in: true }
+    else
+      render json: { error: 'Unable to create user' }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
