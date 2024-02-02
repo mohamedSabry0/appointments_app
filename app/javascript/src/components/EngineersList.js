38d4de82304fb0254fa877dfd8016e751976542b
mohamedSabry0 <mohamedsabry3023@gmail.com>
2024-02-02 14:15:48 +0200

    Implement small screen list navigation 1st draft

--- app/javascript/src/components/EngineersList.js
+++ app/javascript/src/components/EngineersList.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchEngineers, engineersState } from '../redux/engineers/engineersSlice';
 
 function EngineersList() {
   const dispatch = useDispatch();
   const { engineers, error, status } = useSelector(engineersState);
-  console.log('engineers', engineers);
+  const [showId, setShowId] = useState(1);
+
+  const handleShowClick = (prev, id) => {
+    if (id > 0 && id < engineers.length + 1) {
+      document.getElementById(prev).classList.remove('active');
+      document.getElementById(id).classList.add('active');
+      setShowId(id);
+    }
+  };
+
   useEffect(() => {
     if (status === 'idle') {
       dispatch(fetchEngineers());
@@ -15,18 +24,20 @@ function EngineersList() {
   if (status === 'succeeded') {
     return (
       <div className="engineers-list">
+        <button type="button" onClick={() => handleShowClick(showId, showId - 1)}>Previous</button>
         {engineers.map((engineer) => {
           const {
             name, id, photo, speciality,
           } = engineer;
           return (
-            <div key={id}>
+            <div key={id} id={id} className={engineer.id === 1 ? 'active' : ''}>
               <img className="engineer-img" src={photo} alt={name} />
               <p>{name}</p>
               <p>{speciality}</p>
             </div>
           );
         })}
+        <button type="button" onClick={() => handleShowClick(showId, showId + 1)}>Next</button>
       </div>
     );
   }
