3c5906aa00a372d86b5a6c0723e9ac28587831bb
MohammadYaser <yasersafi19@outlook.com>
2024-02-02 15:48:03 +0430

    add bold class to the nav links

--- app/javascript/src/pages/Sidebar.js
+++ app/javascript/src/pages/Sidebar.js
@@ -11,27 +11,27 @@ const Sidebar = () => {
         <ul className='nav nav-pills flex-column p-0 mt-5'>
           <li className='nav-item p-1'>
             <NavLink to="/" className='nav-link' activeClassName='active-link'>
-              <span className='fs-6'>Engineers</span>
+              <span className='fs-6 fw-bold'>Engineers</span>
             </NavLink>
           </li>
           <li className='nav-item p-1'>
             <NavLink to="consultateEngineer" className='nav-link' activeClassName='active-link'>
-              <span className='fs-6'>Consultate Engineer</span>
+              <span className='fs-6 fw-bold'>Consultate Engineer</span>
             </NavLink>
           </li>
           <li className='nav-item p-1'>
             <NavLink to="myConsultation" className='nav-link'>
-              <span className='fs-6'>My Consultation</span>
+              <span className='fs-6 fw-bold'>My Consultation</span>
             </NavLink>
           </li>
           <li className='nav-item p-1'>
             <NavLink to="addEngineer" className='nav-link'>
-              <span className='fs-6'>Add Engineer</span>
+              <span className='fs-6 fw-bold'>Add Engineer</span>
             </NavLink>
           </li>
           <li className='nav-item p-1'>
             <NavLink to="deleteEngineer" className='nav-link'>
-              <span className='fs-6'>Delete Engineer</span>
+              <span className='fs-6 fw-bold'>Delete Engineer</span>
             </NavLink>
           </li>
         </ul>
