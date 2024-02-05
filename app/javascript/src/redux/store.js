8bf59b698ece4a9bdbd6757269a35807b75167e0
erickma1 <110662611+erickma1@users.noreply.github.com>
2024-02-05 20:45:06 +0000

    Update store.js

--- app/javascript/src/redux/store.js
+++ app/javascript/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import engineersReducer from './engineers/engineersSlice';
+import authReducer from './auth/authSlice';
 
 const store = configureStore({
   reducer: {
     engineers: engineersReducer,
+    auth: authReducer,
   },
 });
 
