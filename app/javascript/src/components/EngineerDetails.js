b0940bfc87074b8b9c6797a86a34e75f0371f386
MohammadYaser <yasersafi19@outlook.com>
2024-02-05 15:57:56 +0430

    add some basic layout to engineer details page

--- app/javascript/src/components/EngineerDetails.js
+++ app/javascript/src/components/EngineerDetails.js
@@ -38,7 +38,51 @@ const EngineerDetails = () => {
     );
   }
   return (
-   
+    <>
+      <div class="container py-4">
+        <div class="row align-items-md-stretch">
+          <div class="col-md-6">
+            <div class="h-200 p-5">
+              <img src={photo} alt="Engineer" />
+            </div>
+          </div>
+
+          <div class="col-md-6 details">
+            <div class="h-100 p-5">
+              <h2 className="fw-bolder text-end">{name}</h2>
+              <div className="px-2 mb-2 bg-light bg-gradient text-dark">
+                <span>Consultancy fee:</span>
+                <span className="float-end">&euro;{consultancy_fee}</span>
+              </div>
+              <div className="px-2 mb-2 text-dark">
+                <span>Speciality:</span>
+                <span className="float-end">{speciality}</span>
+              </div>
+              <div className="px-2 mb-2 bg-light bg-gradient text-dark">
+                <span className="about">About:{about}</span>
+                          </div>
+                          <div className="float-end">
+                              
+                              <Link to={"/"} className="fw-bolder carousel-btn reserve-btn">
+                                  <span className="fs-5 setting-icon">
+                                      
+                                  <i class="bi bi-gear"></i>
+                                  </span>
+                                  Reserve
+                                  <span className="fs-5 right-arrow">
+                                      
+                                  <i class="bi bi-arrow-right-circle"></i>
+                                  </span>
+              </Link>
+                          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Link to={"/"} className="carousel-btn back-btn">
+        <i className="bi bi-caret-left float-end" />
+      </Link>
+    </>
   );
 };
 
