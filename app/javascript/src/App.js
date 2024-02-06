31a257299d6bec27ce05906229f190f7b50e19e4
erickma1 <110662611+erickma1@users.noreply.github.com>
2024-02-06 09:36:00 +0000

    Update App.js

--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -9,17 +9,29 @@ import Register from './components/auth/register';
 import Login from './components/auth/login';
 
 function App() {
+  const loggedIn = localStorage.getItem('user');
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Sidebar />}>
-          <Route index element={<HomePage />} />
-          <Route path="consultateEngineer" element={<HomePage />} />
-          <Route path="myConsultation" element={<HomePage />} />
-          <Route path="addEngineer" element={<AddEngineerPage />} />
-          <Route path="deleteEngineer" element={<HomePage />} />
+          {!loggedIn && (
+            <>
+              <Route path="/register" element={<Register />} />
+              <Route index element={<Login />} />
+            </>
+          ) }
+          {
+            loggedIn && (
+              <>
+                <Route index element={<HomePage />} />
+                <Route path="consultateEngineer" element={<HomePage />} />
+                <Route path="myConsultation" element={<HomePage />} />
+                <Route path="addEngineer" element={<AddEngineerPage />} />
+                <Route path="deleteEngineer" element={<HomePage />} />
+              </>
+            )
+          }
         </Route>
       </Routes>
     </BrowserRouter>
