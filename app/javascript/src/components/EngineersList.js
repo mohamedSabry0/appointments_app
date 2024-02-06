a506b927553248e7af272c6b156a9e90a0888ed9
MohammadYaser <yasersafi19@outlook.com>
2024-02-06 22:02:38 +0430

    fix linter error

--- app/javascript/src/components/EngineersList.js
+++ app/javascript/src/components/EngineersList.js
@@ -60,11 +60,13 @@ function EngineersList({ showDeleteButton = false }) {
 
   if (status === 'succeeded') {
     if (!Array.isArray(engineers) || engineers.length === 0) {
-      return <div className="empty-div">
-        <p className='empty-msg'>
-        There are no engineers to display.
-        </p>
-      </div>;
+      return (
+        <div className="empty-div">
+          <p className="empty-msg">
+            There are no engineers to display.
+          </p>
+        </div>
+      );
     }
 
     return (
