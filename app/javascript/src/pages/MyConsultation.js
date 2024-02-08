1d4d14880163befe9c2591d1f18b3801f5c9df7e
MohammadYaser <yasersafi19@outlook.com>
2024-02-08 17:45:29 +0430

    Create MyConsulation page

---
+++ app/javascript/src/pages/MyConsultation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ConsultationList from "../components/ConsultationList";
+const MyConsultation = () => (
+  <div className="home-main">
+    <h1 className="heading">My Consultations</h1>
+    <ConsultationList />
+  </div>
+);
+
+export default MyConsultation;
