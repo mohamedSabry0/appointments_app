21c78e5aeb915c5d459e36eb53e6110ab5fe8048
MohammadYaser <yasersafi19@outlook.com>
2024-02-08 17:44:55 +0430

    Display the user name and engineer name instead of its ids in controller

--- app/controllers/api/v1/consultations_controller.rb
+++ app/controllers/api/v1/consultations_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::ConsultationsController < ApplicationController
+  def index
+    @consultations = Consultation.includes(:user, :engineer).select(:id, :user_id, :engineer_id, :city, :date).all
+    if @consultations.empty?
+      render json: { message: 'No consultations found' }
+    else
+      # Serialize the consultations data to include user and engineer names
+      consultations_data = @consultations.map do |consultation|
+        {
+          id: consultation.id,
+          user_name: consultation.user&.username, # Get user name if present
+          engineer_name: consultation.engineer&.name, # Get engineer name if present
+          city: consultation.city,
+          date: consultation.date
+        }
+      end
+      render json: consultations_data
+    end
+  end
   
+
   def create
     @consultation = Consultation.new(consultation_params)
     if @consultation.save
