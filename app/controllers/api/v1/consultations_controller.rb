bf4dc7a8a4b94c2a70c3d00b88e7d4263ed42921
MohammadYaser <yasersafi19@outlook.com>
2024-02-08 17:42:57 +0430

    add route for my consulation page

--- app/controllers/api/v1/consultations_controller.rb
+++ app/controllers/api/v1/consultations_controller.rb
@@ -1,13 +1,5 @@
 class Api::V1::ConsultationsController < ApplicationController
-  def index
-    @consultations = Consultation.select(:id, :user_id, :engineer_id, :city, :date).all
-    if @consultations.empty?
-      render json: { message: 'No consultations found' }
-    else
-      render json: @consultations
-    end
-  end
-
+  
   def create
     @consultation = Consultation.new(consultation_params)
     if @consultation.save
--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -10,6 +10,7 @@ import DeleteEngineer from './pages/DeleteEngineer';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
 import AddReservationPage from './pages/AddReservationPage';
+import MyConsultation from './pages/MyConsultation';
 
 function App() {
   const token = JSON.parse(localStorage.getItem('token'), '');
@@ -25,7 +26,7 @@ function App() {
               <Route path="engineers" element={<HomePage />} />
               <Route path="/engineersList/:engineerId" element={<EngineerDetails />} />
               <Route path="consultateEngineer" element={<AddReservationPage />} />
-              <Route path="myConsultation" element={<HomePage />} />
+              <Route path="myConsultation" element={<MyConsultation />} />
               <Route path="addEngineer" element={<AddEngineerPage />} />
               <Route path="deleteEngineer" element={<DeleteEngineer />} />
 
